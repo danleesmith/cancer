929f7d595a064f7b5151306e77292ea389a036df
Dan Smith <danleesmith86@gmail.com>
2022-03-02 15:51:48 +1000

    implement moment struct

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,68 @@
-fn julian_day(mut year: f64, mut month: f64, day: f64, hour: f64, minute: f64, second: f64, millisecond: f64) -> f64 {
-
-	let doty = day + (hour / 24.0) + (minute / 1440.0) + (second + millisecond / 1000.0) / 86400.0;
-	let mut b = 0.0;
-
-	if month == 1.0 || month == 2.0 {
-		year -= 1.0;
-		month += 12.0;
-	}
-
-	if !is_julian_date(year, month, day) {
-		let a = (year / 100.0).floor();
-		b = 2.0 - (a / 4.0).floor();
-	}
-
-	(365.25 * (year + 4716.0)).floor() + (30.6001 * (month + 1.0)).floor() + doty + b - 1524.5
-
+struct Moment {
+	year: i32,
+	month: i32, 
+	day: i32, 
+	hour: i32, 
+	minute: i32, 
+	second: i32, 
+	millisecond: i32 ,
 }
 
-fn is_julian_date(year: f64, month: f64, day: f64) -> bool {
-	let mut cond = true;
-	if year > 1582.0 { cond = false };
-	if year < 1582.0 { cond = true };
-	if month > 10.0 { cond = false };
-	if month < 10.0 { cond = true };
-	if day > 14.0 { cond = false };
-	cond
+impl Moment {
+	fn new (y: i32, m: i32, d: i32, hr: i32, mn: i32, sc: i32, ms: i32) -> Moment {
+		Moment {
+			year: y,
+			month: m, 
+			day: d, 
+			hour: hr, 
+			minute: mn, 
+			second: sc, 
+			millisecond: ms,
+		}
+	}
+
+	fn day_of_month(&self) -> f64 {
+		self.day as f64 + 
+		(self.hour as f64 / 24.0) + 
+		(self.minute as f64 / 1440.0) + 
+		(self.second as f64 + self.millisecond as f64 / 1000.0) / 86400.0
+	}
+
+	fn is_julian_date(&self) -> bool {
+		let mut rtn = true;
+		if self.year > 1582 { rtn = false };
+		if self.year < 1582 { rtn = true };
+		if self.month > 10 { rtn = false };
+		if self.month < 10 { rtn = true };
+		if self.day > 14 { rtn = false };
+		rtn
+	}
+
+	fn julian_day(&self) -> f64 {
+		let mut y = self.year;
+		let mut m = self.month;
+		let mut b = 0.0;
+
+		if m == 1 || m == 2 {
+			y -= 1;
+			m += 12;
+		}
+
+		if !self.is_julian_date() {
+			let a = (y as f64 / 100.0).trunc();
+			b = 2.0 - a + (a / 4.0).trunc();
+		}
+
+		let jd = (365.25 * (y + 4716) as f64).trunc() + (30.6001 * (m + 1) as f64).trunc() + self.day_of_month() + b - 1524.5;
+		
+		jd
+	}
 }
 
 fn main() {
-    println!("JD: {}", julian_day(1957.0, 10.0, 4.0, 19.0, 29.0, 0.0, 0.0));
+	let moment = Moment::new(1957, 10, 4, 19, 29, 0, 0);
+    println!("JD: {}", moment.julian_day());
+	println!("JD?: {}", moment.is_julian_date());
+	println!("DoM: {}", moment.day_of_month());
+
 }
