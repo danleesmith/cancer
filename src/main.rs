4b75040ea53a6568e705121a59277c84ec4e94b0
Dan Smith <danleesmith86@gmail.com>
2022-03-03 14:20:15 +1000

    refactoring and tests

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,30 @@
 mod moment;
-use crate::moment::Moment;
+use crate::moment::*;
+
+use::std::collections::HashMap;
 
 fn main() {
-	let moment = Moment::new(1957, 10, 4, 19, 29, 0, 0);
-    println!("JD: {}", moment.julian_day());
-	println!("JD?: {}", moment.is_julian_date());
-	println!("DoM: {}", moment.day_of_month());
 
+	let jd_test = HashMap::from([
+		("Sputnik Launch", julian_day(1957, 10, 4, 19, 29, 0)),
+		("2000 Jan. 1.5", julian_day(2000, 1, 1, 12, 0, 0)),
+		("1999 Jan. 1.0", julian_day(1999, 1, 1, 0, 0, 0)),
+		("1987 Jan. 27.0", julian_day(1987, 1, 27, 0, 0, 0)),
+		("1987 Jun. 19.5", julian_day(1987, 6, 19, 12, 0, 0)),
+		("1988 Jan. 27.0", julian_day(1988, 1, 27, 0, 0, 0)),
+		("1900 Jan. 1.0", julian_day(1900, 1, 1, 0, 0, 0)),
+		("1600 Jan. 1.0", julian_day(1600, 1, 1, 0, 0, 0)),
+		("1600 Dec. 31.0", julian_day(1600, 12, 31, 0, 0, 0)),
+		("837 Apr. 10.3", julian_day(837, 4, 10, 7, 12, 0)),
+		("-123 Dec. 31.0", julian_day(-123, 12, 31, 0, 0, 0)),
+		("-122 Jan. 1.0", julian_day(-122, 1, 1, 0, 0, 0)),
+		("-1000 July. 12.5", julian_day(-1000, 7, 12, 12, 0, 0)),
+		("-1000 Feb. 29.0", julian_day(-1000, 2, 29, 0, 0, 0)),
+		("-1001 Aug. 17.9", julian_day(-1001, 9, 17, 21, 36, 0)),
+		("-4712 Jan. 1.5", julian_day(-4712, 1, 1, 12, 0, 0)),
+	]);
+
+	for (date, jd) in &jd_test {
+		println!("Date: {} | Julian Date: {}", date, jd);
+	}
 }
--- src/moment.rs
+++ src/moment.rs
@@ -1,60 +1,31 @@
-pub struct Moment {
-	year: i32,
-	month: i32, 
-	day: i32, 
-	hour: i32, 
-	minute: i32, 
-	second: i32, 
-	millisecond: i32 ,
+const DAY_S: f64 = 86400.0;
+
+pub fn is_julian_date(year: i32, month: u32, day: u32) -> bool {
+	let mut rtn = true;
+	if year > 1582 { rtn = false };
+	if year < 1582 { rtn = true };
+	if month > 10 { rtn = false };
+	if month < 10 { rtn = true };
+	if day > 14 { rtn = false };
+	return rtn
 }
 
-impl Moment {
-	pub fn new (y: i32, m: i32, d: i32, hr: i32, mn: i32, sc: i32, ms: i32) -> Moment {
-		Moment {
-			year: y,
-			month: m, 
-			day: d, 
-			hour: hr, 
-			minute: mn, 
-			second: sc, 
-			millisecond: ms,
-		}
+pub fn julian_day(mut year: i32, mut month: u32, day: u32, hour: u32, minute: u32, second: u32) -> f64 {
+	let mut b = 0.0;
+
+	if month <= 2 {
+		year -= 1;
+		month += 12;
 	}
 
-	pub fn day_of_month(&self) -> f64 {
-		self.day as f64 + 
-		(self.hour as f64 / 24.0) + 
-		(self.minute as f64 / 1440.0) + 
-		(self.second as f64 + self.millisecond as f64 / 1000.0) / 86400.0
+	if !is_julian_date(year, month, day) {
+		let a = (year as f64 / 100.0).trunc();
+		b = 2.0 - a + (a / 4.0).trunc();
 	}
 
-	pub fn is_julian_date(&self) -> bool {
-		let mut rtn = true;
-		if self.year > 1582 { rtn = false };
-		if self.year < 1582 { rtn = true };
-		if self.month > 10 { rtn = false };
-		if self.month < 10 { rtn = true };
-		if self.day > 14 { rtn = false };
-		rtn
-	}
+	let d = day as f64 + (hour as f64 / 24.0) + (minute as f64 / 1440.0) + (second as f64 / 1000.0) / DAY_S;
 
-	pub fn julian_day(&self) -> f64 {
-		let mut y = self.year;
-		let mut m = self.month;
-		let mut b = 0.0;
-
-		if m == 1 || m == 2 {
-			y -= 1;
-			m += 12;
-		}
-
-		if !self.is_julian_date() {
-			let a = (y as f64 / 100.0).trunc();
-			b = 2.0 - a + (a / 4.0).trunc();
-		}
-
-		let jd = (365.25 * (y + 4716) as f64).trunc() + (30.6001 * (m + 1) as f64).trunc() + self.day_of_month() + b - 1524.5;
-		
-		jd
-	}
+	let jd = (365.25 * (year + 4716) as f64).trunc() + (30.6001 * (month + 1) as f64).trunc() + d + b - 1524.5;
+	
+	return jd
 }
